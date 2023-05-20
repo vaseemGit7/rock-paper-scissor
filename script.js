c586fb907887f060912b6ed7767171acf1fa251d
vaseemGit7 <105648020+vaseemGit7@users.noreply.github.com>
2023-05-20 12:23:14 +0530

    Make the game play

--- script.js
+++ script.js
@@ -1,13 +1,55 @@
-let playerSelection = prompt("Choose either rock, paper or scissor");
-playerSelection.toLowerCase();
+function playerSelection(){
+    let playerChoice = prompt("Choose either rock, paper or scissor");
+    return playerChoice.toLowerCase();
+}
 
 function computerSelection(){
-    const choices = ['rock','paper','scisscor'];
+    const choices = ['rock','paper','scissor'];
     
     let choice = Math.floor(Math.random()*3);
 
     let selection = choices[choice];
 
     return selection;
+}
+
+function playRound(playerSelection,computerSelection){
+    if(playerSelection=='rock'&&computerSelection=='scissor'){
+        return "You win!,Rock beats Scissor";
+    }
+    else if(playerSelection=='rock'&&computerSelection=='paper'){
+        return "You lose!,Paper beats Rock";
+    }
+    else if(playerSelection=='paper'&&computerSelection=='rock'){
+        return "You win!,Paper beats Rock";
+    }
+    else if(playerSelection=='paper'&&computerSelection=='scissor'){
+        return "You lose!,Scissor beats Paper";
+    }
+    else if(playerSelection=='scissor'&&computerSelection=='paper'){
+        return "You win!,Scissor beats Paper";
+    }
+    else if(playerSelection=='scissor'&&computerSelection=='rock'){
+        return "You lose!,Rock beats Scissor";
+    }
+    else if(playerSelection=='rock'&&computerSelection=='rock'){
+        return "Draw!, Play again!"
+    }
+    else if(playerSelection=='scissor'&&computerSelection=='scissor'){
+        return "Draw!, Play again!"
+    }
+    else if(playerSelection=='paper'&&computerSelection=='paper'){
+        return "Draw!, Play again!"
+    }
+    else{
+        return "Enter correctly!.";
+    }
+}
+
+let rounds = 5;
+
+while(rounds>0){
+    console.log(playRound(playerSelection(),computerSelection()));
+    rounds--;
 }
 
