55994ae372c700c446671bd27c9c7a80bffb6f5e
vaseemGit7 <105648020+vaseemGit7@users.noreply.github.com>
2023-06-17 22:14:31 +0530

    Add choices and final message

--- script.js
+++ script.js
@@ -39,25 +39,36 @@ function playRound(playerSelection,computerSelection){
 
 }
 
+function isGameOver(){
+    return playerScore==5 || computerScore ==5 ;
+}
+
 const resultHeader = document.getElementById("resultHeader");
 const resultMessage = document.getElementById("resultMessage");
 const playerChoice = document.getElementById("playerChoice");
 const enemyChoice = document.getElementById("enemyChoice");
 const playerWins = document.getElementById("playerScore");
 const enemyWins = document.getElementById("enemyScore");
+const resultAnnouncement =document.getElementById("resultAnnouncement");
 
 const rockBtn = document.getElementById("rock");
 const paperBtn = document.getElementById("paper");
 const scissorBtn = document.getElementById("scissors");
+const resetBtn = document.getElementById("resetBtn");
 
 rockBtn.addEventListener('click',() => handleClick('rock'));
 paperBtn.addEventListener('click',() => handleClick('paper'));
 scissorBtn.addEventListener('click',() => handleClick('scissor'));
+resetBtn.addEventListener('click',resetGame);
 
 function handleClick(playerSelection){
+
     const computerSelection = getComputerSelection();
     playRound(playerSelection,computerSelection);
+    updateScoreMessage(playerSelection,computerSelection);
     updateScore();
+    updateChoice(playerSelection,computerSelection);
+    
     
 }
 
@@ -76,6 +87,48 @@ function updateScore(){
     enemyWins.textContent = computerScore;
 }
 
+function updateScoreMessage(playerSelection,computerSelection){
+    if(roundWinner === 'tie'){
+        resultMessage.textContent ="Play Again!";
+    }
+    else if(roundWinner === 'player'){
+        resultMessage.textContent = `${capitalize(playerSelection)} beats ${computerSelection}`;
+    }
+    else if(roundWinner === 'computer'){
+        resultMessage.textContent =`${capitalize(playerSelection)} is beaten by ${computerSelection}`;
+    }
+}
 
+function capitalize(string){
+    return string.charAt(0).toUpperCase()+string.slice(1).toLowerCase();
+}
 
+function updateChoice(playerSelection,computerSelection){
+    switch(playerSelection){
+        case 'rock':
+            playerChoice.setAttribute('alt','rock');
+            break;
+        case 'paper':
+            playerChoice.setAttribute('alt','paper');
+            break;
+        case 'scissor':
+            playerChoice.setAttribute('alt','scissor');
+            break;        
+    }
 
+    switch(computerSelection){
+        case 'rock':
+            enemyChoice.setAttribute('alt','rock');
+            break;
+        case 'paper':
+            enemyChoice.setAttribute('alt','paper');
+            break;
+        case 'scissor':
+            enemyChoice.setAttribute('alt','scissor');
+            break;
+    }
+}
+
+function resetGame(){
+    window.location.reload();
+}
