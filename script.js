c7be2eaea86906a3ccc443496f0ad9d62f7a5279
vaseemGit7 <105648020+vaseemGit7@users.noreply.github.com>
2023-06-17 18:45:35 +0530

    Add button and scoreboard manipulation

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ let playerScore = 0;
 let computerScore = 0;
 let roundWinner = '';
 
-function computerSelection(){
+function getComputerSelection(){
     const choices = ['rock','paper','scissor'];
     
     let choice = Math.floor(Math.random()*3);
@@ -17,6 +17,7 @@ function playRound(playerSelection,computerSelection){
 
     if(playerSelection==computerSelection){
         roundWinner ='tie';
+        console.log('tie');
     }
 
     if((playerSelection=='rock'&&computerSelection=='scissor') ||
@@ -25,6 +26,7 @@ function playRound(playerSelection,computerSelection){
         ){
         playerScore++;
         roundWinner ='player';
+        console.log('player');
     }
 
     if((playerSelection=='rock'&&computerSelection=='paper')||
@@ -32,8 +34,46 @@ function playRound(playerSelection,computerSelection){
         (playerSelection=='scissor'&&computerSelection=='rock')){
         computerScore++;
         roundWinner ='computer';
+        console.log('enemy')
     }
 
+}
+
+const resultHeader = document.getElementById("resultHeader");
+const resultMessage = document.getElementById("resultMessage");
+const playerChoice = document.getElementById("playerChoice");
+const enemyChoice = document.getElementById("enemyChoice");
+const playerWins = document.getElementById("playerScore");
+const enemyWins = document.getElementById("enemyScore");
+
+const rockBtn = document.getElementById("rock");
+const paperBtn = document.getElementById("paper");
+const scissorBtn = document.getElementById("scissors");
+
+rockBtn.addEventListener('click',() => handleClick('rock'));
+paperBtn.addEventListener('click',() => handleClick('paper'));
+scissorBtn.addEventListener('click',() => handleClick('scissor'));
+
+function handleClick(playerSelection){
+    const computerSelection = getComputerSelection();
+    playRound(playerSelection,computerSelection);
+    updateScore();
+    
+}
+
+function updateScore(){
+    if(roundWinner === 'tie'){
+        resultHeader.textContent = "It's a Tie!"
+    }
+    if(roundWinner === 'player'){
+        resultHeader.textContent = "You Won!"
+    }
+    if(roundWinner === 'computer'){
+        resultHeader.textContent = "You Lost!"
+    }
+
+    playerWins.textContent = playerScore;
+    enemyWins.textContent = computerScore;
 }
 
 
