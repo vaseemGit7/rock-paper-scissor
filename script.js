87bec301a7a0aeb02dda0a8a8ba53eb7ba61dc08
vaseemGit7 <105648020+vaseemGit7@users.noreply.github.com>
2023-06-17 14:46:23 +0530

    Refactor script file

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
-function playerSelection(){
-    let playerChoice = prompt("Choose either rock, paper or scissor");
-    return playerChoice.toLowerCase();
-}
+let playerScore = 0;
+let computerScore = 0;
+let roundWinner = '';
 
 function computerSelection(){
     const choices = ['rock','paper','scissor'];
@@ -13,73 +12,30 @@ function computerSelection(){
     return selection;
 }
 
-let playerScore = 0;
-let computerScore = 0;
 
 function playRound(playerSelection,computerSelection){
 
-    if(playerSelection=='rock'&&computerSelection=='scissor'){
+    if(playerSelection==computerSelection){
+        roundWinner ='tie';
+    }
+
+    if((playerSelection=='rock'&&computerSelection=='scissor') ||
+        (playerSelection=='paper'&&computerSelection=='rock') ||
+        (playerSelection=='scissor'&&computerSelection=='paper')
+        ){
         playerScore++;
-        return ["player score : " + playerScore,"You win!,Rock beats Scissor"];
+        roundWinner ='player';
     }
 
-    else if(playerSelection=='rock'&&computerSelection=='paper'){
+    if((playerSelection=='rock'&&computerSelection=='paper')||
+        (playerSelection=='paper'&&computerSelection=='scissor')||
+        (playerSelection=='scissor'&&computerSelection=='rock')){
         computerScore++;
-        return ["computer score : " + computerScore,"You lose!,Paper beats Rock"];
+        roundWinner ='computer';
     }
 
-    else if(playerSelection=='paper'&&computerSelection=='rock'){
-        playerScore++;
-        return ["player score : " + playerScore,"You win!,Paper beats Rock"];
-    }
-
-    else if(playerSelection=='paper'&&computerSelection=='scissor'){
-        computerScore++;
-        return ["computer score : " + computerScore,"You lose!,Scissor beats Paper"];
-    }
-
-    else if(playerSelection=='scissor'&&computerSelection=='paper'){
-        playerScore++;
-        return ["player score : " + playerScore,"You win!,Scissor beats Paper"];
-    }
-
-    else if(playerSelection=='scissor'&&computerSelection=='rock'){
-        computerScore++;
-        return ["computer score : " + computerScore,"You lose!,Rock beats Scissor"];
-    }
-
-    else if(playerSelection=='rock'&&computerSelection=='rock'){
-        return "Draw!, Play again!"
-    }
-
-    else if(playerSelection=='scissor'&&computerSelection=='scissor'){
-        return "Draw!, Play again!"
-    }
-
-    else if(playerSelection=='paper'&&computerSelection=='paper'){
-        return "Draw!, Play again!"
-    }
-
-    else{
-        return "Enter correctly!.";
-    }
 }
 
 
-function game(){
-    let rounds = 5;
 
-    while(rounds>0){
-        console.log(playRound(playerSelection(),computerSelection()));
-        rounds--;
-    }
 
-    if(playerScore>computerScore){
-        console.log("Player wins!")
-    }
-    else if(computerScore>playerScore){
-        console.log("Computer wins!")
-    }
-}
-
-game();
